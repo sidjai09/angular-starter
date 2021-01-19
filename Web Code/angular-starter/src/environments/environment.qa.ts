e5a9118e367750137fa91723f8c8711db482da4c
Siddharth Jaiswal <46618565+sidjai09@users.noreply.github.com>
2021-01-20 01:22:05 +0530

    Commit for QA Environment Setup

---
+++ Web Code/angular-starter/src/environments/environment.qa.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+  platform: 'qa',
+  serverUrl: 'QA API URL GOES Here',
+  clientUrl: 'QA CLIENT URL GOES Here',
+  version: '1.0',
+  Date: '2021'
+};
