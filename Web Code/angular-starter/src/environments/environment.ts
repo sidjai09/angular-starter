f07f13d8fcc115e2f239d0521e67ba1e86df97f7
Siddharth Jaiswal <46618565+sidjai09@users.noreply.github.com>
2021-01-20 00:55:24 +0530

    Setup Local Environment

--- Web Code/angular-starter/src/environments/environment.ts
+++ Web Code/angular-starter/src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  platform: 'local',
+  serverUrl: 'localhost/serverURL',
+  clientUrl: 'localhost:4200',
+  version: '1.0',
+  Date: '2021'
 };
 
 /*
