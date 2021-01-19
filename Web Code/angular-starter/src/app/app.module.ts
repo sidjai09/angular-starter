b5ba891c089234b2a5fa43b540d3a266c93c6a8a
Siddharth Jaiswal <46618565+sidjai09@users.noreply.github.com>
2021-01-20 01:58:04 +0530

    Setup PWA

--- Web Code/angular-starter/src/app/app.module.ts
+++ Web Code/angular-starter/src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})],
   providers: [],
   bootstrap: [AppComponent]
 })
